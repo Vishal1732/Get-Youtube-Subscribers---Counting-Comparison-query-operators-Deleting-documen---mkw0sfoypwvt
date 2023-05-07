60c58666ec813fa8581b1c0fd19e6e8716348c21
Vishal Dubey <92910196+Vishal1732@users.noreply.github.com>
2023-05-07 23:31:43 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,28 +1,43 @@
-
 const express = require('express');
-const app = express()
+const router = express.Router();
+const Subscriber = require('./models/subscribers');
 
+// GET /subscribers
+router.get('/subscribers', async (req, res) => {
+  try {
+    const subscribers = await Subscriber.find();
+    res.send(subscribers);
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+});
 
-// Your code goes here
+// GET /subscribers/names
+router.get('/subscribers/names', async (req, res) => {
+  try {
+    const subscribers = await Subscriber.find({}, { name: 1, subscribedChannel: 1 });
+    res.send(subscribers);
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+});
 
+// GET /subscribers/:id
+router.get('/subscribers/:id', getSubscriber, (req, res) => {
+  res.send(res.subscriber);
+});
 
+async function getSubscriber(req, res, next) {
+  try {
+    const subscriber = await Subscriber.findById(req.params.id);
+    if (subscriber == null) {
+      return res.status(400).send({ message: 'Cannot find subscriber' });
+    }
+    res.subscriber = subscriber;
+    next();
+  } catch (err) {
+    return res.status(500).send({ message: err.message });
+  }
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-module.exports = app;
+module.exports = router;
