15eaf7305c63a4d9895997bdd87effac168ae1c4
Vishal Dubey <92910196+Vishal1732@users.noreply.github.com>
2023-05-08 18:04:36 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,39 +1,36 @@
 const express = require('express');
-const app = express();
-const mongoose = require('mongoose');
-const { off } = require('../models/product.js');
-const products   =require("../models/product.js");
+const app = express()
 
-// Import routes
-
-//Router Middlewares
-app.use(express.json());
+const Subscriber = require('./models/subscribers');
+const ObjectId = require("mongoose");
 
 
-//default value for limit is 5 and offset is 0
-//This route should return an array of _id of all the element that need to be rturned.
-//output id can be in any order.
-
-app.get("/",async function(req,res){
-
-    var limit=req.query.limit,offset=req.query.offset;
-
-    if(limit == null) limit = 5;
-    else limit = parseInt(limit);
-    if(offset == null) offset=0;
-    else offset = parseInt(offset);
-
-    if(limit > 5) limit = 5;
-
-    result = await products.find({});
-
-    var ids = [];
-    const startt = (limit*offset);
-
-    for(var i = startt; i < result.length && i < (startt+limit) ; i++) ids.push(result[i]["_id"]);
-
-    res.send(ids);
-
+app.get('/subscribers', (req, res) => {
+    Subscriber.find().then(subscribers => res.send(subscribers));
+    return;
 });
+app.get('/subscribers/names', (req, res) => {
+    Subscriber.find().select({name: 1, subscribedChannel: 1}).then(subscribers => res.send(subscribers));
+    return;
+});
+app.get('/subscribers/:id', (req, res) => {
+    const id = req.params.id;
+    Subscriber.find({_id : id}).then(subscribers => subscribers.map(subscribers => res.send(subscribers))).catch(error => res.status(400).send({message: error.message}));
+    return;
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 module.exports = app;
